60c5508fc034b57450cbf64349dc79318d7d9771
Botond Horvath <hello@boti7.com>
2020-01-06 22:54:49 +0100

    Set up Vuetify and Leaflet

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-named-as-default': 'off',
+    'class-methods-use-this': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
---
+++ src/types/vue2-leaflet.d.ts
@@ -0,0 +1,6 @@
+declare module 'vue2-leaflet' {
+  import * as L from 'leaflet';
+
+  // eslint-disable-next-line import/prefer-default-export
+  export { L };
+}
